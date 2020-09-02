c71e466d12c5d4e4009fca4f674d549a34ba8607
Adityaacharya1807 <aditya.acharya@moolya.com>
2020-09-02 10:33:48 +0530

    Pdf func added

--- tests/example.test.js
+++ tests/example.test.js
@@ -29,6 +29,7 @@ describe("Automation Test for Zero Web App", () => {
      it("Feedback page", async () => {
         await feedback.visitFeedback()
         await  feedback.feedbackData('aditya','anshu.ach18@gmail.com','Hello','Whats your name ?')    
+        //await  page.pdf({path:"example.pdf", format:"A4"})// Browser Should be in headless mode
     })
       it("Login page", async () => {
          await loginpage.visit()
